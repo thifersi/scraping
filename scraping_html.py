f44daf8da91cde2aa642be30ef1891c7c5e05aed
thifersi <tfsparticular@gmail.com>
2024-09-30 13:16:29 -0300

    scraping_html.py

--- scraping_html.py
+++ scraping_html.py
@@ -1,6 +1,6 @@
 """
-Este script utiliza Selenium para fazer scraping de uma página web
-usando o ChromeDriver e captura o conteúdo HTML da página.
+Este script utiliza Selenium para acessar uma página web, extrair dados de empresas e
+exportá-los para uma planilha Excel usando a biblioteca pandas.
 """
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -19,22 +19,30 @@ driver = webdriver.Chrome(service=service)
 driver.get('https://www.listamais.com.br/busca?pagina=2&ordemNumero=&inicio=10&termo=Advocacia&campo=todos&localizacao=&codigo=520')
 
 # Aguardar a página carregar completamente
-WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'resultado-busca-gratuito')))
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'resultado-busca-gratuito')))
 
-# Captura o HTML após o carregamento completo e execução do JavaScript
+# Captura o HTML após o carregamento
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
 # Encontrar os blocos de informações de empresas
 companies = []
-for company in soup.find_all('div', class_='resultado-busca-gratuito'):
+empresas = soup.find_all('div', class_='resultado-busca-gratuito')
+
+# Captura todos os números de telefone na classe 'idAnalytics'
+telefones = soup.find_all('div', class_='idAnalytics')
+
+# Iterar por cada empresa e associar com o número de telefone
+for idx, company in enumerate(empresas):
     nome = company.find('h2', class_='bd-name-empresa').text.strip()
     endereco = company.find('div', class_='endereco-empresa-gratuito').text.strip()
-    # Capturar o número da classe 'idAnalytics', mesmo com display:none
-    try:
-        telefone = company.find('div', class_='idAnalytics').text.strip()
-    except AttributeError:
+
+    # Verifica se o índice do telefone é válido
+    if idx < len(telefones):
+        telefone = telefones[idx].text.strip()
+    else:
         telefone = "Telefone não disponível"
+
     companies.append({
         'Nome': nome,
         'Endereço': endereco,
