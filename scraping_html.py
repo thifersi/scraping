ae56f16093ee7e9728d3f93a378ff59b878ee894
thifersi <tfsparticular@gmail.com>
2024-09-30 10:35:37 -0300

    scraping_html.py

--- scraping_html.py
+++ scraping_html.py
@@ -4,6 +4,9 @@ usando o ChromeDriver e captura o conteúdo HTML da página.
 """
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import pandas as pd
 
@@ -15,23 +18,29 @@ driver = webdriver.Chrome(service=service)
 # Acessa o site
 driver.get('https://www.listamais.com.br/busca?pagina=2&ordemNumero=&inicio=10&termo=Advocacia&campo=todos&localizacao=&codigo=520')
 
-# Captura o HTML da página
-html = driver.page_source
+# Aguardar a página carregar completamente
+WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'resultado-busca-gratuito')))
 
-# Usa o BeautifulSoup para analisar o HTML
-soup = BeautifulSoup(html, 'lxml')
+# Captura o HTML após o carregamento completo e execução do JavaScript
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
 
 # Encontrar os blocos de informações de empresas
 companies = []
-for company in soup.find_all('id', class_='class_do_bloco_de_empresa'):  # Troque pela classe correta
-    nome = company.find('cli_nome').text  # Troque pela tag correta
-    endereco = company.find('end_completo', class_='endereco').text  # Troque pela classe correta
-    telefone = company.find('telefones_negocios', class_='telefone').text  # Troque pela classe correta
+for company in soup.find_all('div', class_='resultado-busca-gratuito'):
+    nome = company.find('h2', class_='bd-name-empresa').text.strip()
+    endereco = company.find('div', class_='endereco-empresa-gratuito').text.strip()
+    # Capturar o número da classe 'idAnalytics', mesmo com display:none
+    try:
+        telefone = company.find('div', class_='idAnalytics').text.strip()
+    except AttributeError:
+        telefone = "Telefone não disponível"
     companies.append({
         'Nome': nome,
         'Endereço': endereco,
         'Telefone': telefone
     })
+
 # Fecha o navegador
 driver.quit()
 
